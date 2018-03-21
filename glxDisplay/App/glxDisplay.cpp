3d5649c7ae249d39ef84922cab970cf560274698
FrancescoMarchesini <marchesinifrancesco1@gmail.com>
2018-03-21 22:40:27 +0100

    aggiunto funzione call back per mouuse e tastiera

--- glxDisplay/App/glxDisplay.cpp
+++ glxDisplay/App/glxDisplay.cpp
@@ -6,10 +6,6 @@ glxDisplay::glxDisplay()
     mWindow = NULL;
     mWidth = 512;
     mHeight = 512;
-    r = 0.f;
-    g = 0.f;
-    b = 0.4f;
-    a = 0.f;
 }
 
 glxDisplay::~glxDisplay()
@@ -59,8 +55,45 @@ bool glxDisplay::initGL()
 {
     //init glew
     glfwMakeContextCurrent(mWindow);
+
+    //init cursore costum
+    GLFWcursor* cursor = initCostumCursor();
+    glfwSetCursor(mWindow, cursor);
+
     //funzione call back per resize windows
     glfwSetFramebufferSizeCallback(mWindow, glxDisplay::framebuffer_size_callback);
+    //funzione call_back per ottenere cordinate del mouse all'interno dello schermo
+    glfwSetCursorPosCallback(mWindow, glxDisplay::mouse_callback);
+    //funzione call_back per ottenere lo scrollo della rotella
+    glfwSetScrollCallback(mWindow, glxDisplay::scroll_callback);
+    //funzione callback per cursore dentro o fuori la finistra
+    glfwSetCursorEnterCallback(mWindow, cursor_enter_call_back);
+    //funzione callback per tasti mouse
+    glfwSetMouseButtonCallback(mWindow, mouse_button_callback);
+    //funzione callback per ottenere lo stream input
+    glfwSetCharCallback(mWindow, character_callback);
+
+    //NOTARE LA DOPPIA CHIMATA DELLA FUNZIONE NON SO SE PUO DARE PROBLEMI
+    //UNA CHIAMATA E PER IL MOUSE L'ALTRA PER LA TASTIRA
+    /**
+     * @brief glfwSetInputMode listener sugli eventi
+     * args 1 : finesrtra nella quale stare in ascoloto
+     * args 2 : Cosa ascoltare in questo il mouse
+     * args 3 : come ascolotare
+     */
+    glfwSetInputMode(mWindow, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+
+    /**
+     * @brief glfwSetInputMode listener sugli eventi
+     * args 1 : finesrtra nella quale stare in ascoloto
+     * args 2 : Cosa ascoltare in questo caso i tasti premuti
+     * args 3 : alla premitura di un tasto il valore di ritorno è GLFW_PRESS
+     */
+    glfwSetInputMode(mWindow, GLFW_STICKY_KEYS, GL_TRUE);
+
+    glfwSetInputMode(mWindow, GLFW_STICKY_MOUSE_BUTTONS, 1);
+    /////////////////////////////////////////////////////////////
+
     glewExperimental == true;
     if(glewInit() != GLEW_OK)
     {
@@ -99,14 +132,6 @@ glxDisplay* glxDisplay::create()
 
 void glxDisplay::draw()
 {
-    /**
-     * @brief glfwSetInputMode listener sugli eventi
-     * args 1 : finesrtra nella quale stare in ascoloto
-     * args 2 : Cosa ascoltare in questo caso i tasti premuti
-     * args 3 : alla premitura di un tasto il valore di ritorno è GLFW_PRESS
-     */
-    glfwSetInputMode(mWindow, GLFW_STICKY_KEYS, GL_TRUE);
-
     //main loop per diseganre
     while(!glfwWindowShouldClose(mWindow))
     {
@@ -114,14 +139,67 @@ void glxDisplay::draw()
         processInput(mWindow);
 
         //pulisci lo schermo ad ogni loop
-        glClearColor(r, g, b, a);
+        glClearColor(0.f, 0.f, 0.4f, 0.f);
         glClear( GL_COLOR_BUFFER_BIT );
 
         //swap del buffer
         glfwSwapBuffers(mWindow);
-        glfwPollEvents();
+
+        // sempre in ascolto degli eventi
+        //glfwPollEvents();
+        //aggiorna gli eventi nel mouse solo quando ci sono
+        glfwWaitEvents();
     }
     glfwTerminate();
+}
+
+GLFWcursor *glxDisplay::initCostumCursor()
+{
+    unsigned char pixels[16 * 16 * 4];
+    memset(pixels, 0xff, sizeof(pixels));
+    GLFWimage image;
+    image.width = 16;
+    image.height = 16;
+    image.pixels = pixels;
+    GLFWcursor* cursor = glfwCreateCursor(&image, 0, 0);
+    return cursor;
+}
+
+void glxDisplay::mouse_callback(GLFWwindow *window, double xpos, double ypos)
+{
+    printf("%s mouse : xpos = %f ypos = %f\n", LOG_GLFW, xpos, ypos);
+}
+
+void glxDisplay::scroll_callback(GLFWwindow *window, double xoffset, double yoffset)
+{
+    printf("%s scroll : xpos = %f ypos = %f\n", LOG_GLFW, xoffset, yoffset);
+}
+
+void glxDisplay::cursor_enter_call_back(GLFWwindow *window, int entered)
+{
+    if (entered)
+    {
+        printf("%s tu sei dentro \n", LOG_GLFW);
+    }
+    else
+    {
+        printf("%s tu sei fuori \n", LOG_GLFW);
+    }
+}
+
+void glxDisplay::mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+    if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
+         printf("%s Tasto destro mouse \n", LOG_GLFW);
+    if (button == GLFW_MOUSE_BUTTON_MIDDLE && action == GLFW_PRESS)
+         printf("%s Tasto centrale mouse \n", LOG_GLFW);
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+         printf("%s Tasto sinistro mouse \n", LOG_GLFW);
+}
+
+void glxDisplay::character_callback(GLFWwindow* window, unsigned int codepoint)
+{
+    printf("%s tasto unicode : %d \n", LOG_GLFW, codepoint);
 }
 
 void glxDisplay::processInput(GLFWwindow *window)
--- glxDisplay/App/glxDisplay.h
+++ glxDisplay/App/glxDisplay.h
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -40,6 +42,15 @@ private:
     bool initGL();
 
     /**
+     * @brief initCostumCursor funzione nerd per cambiare l'apparenza del cursore
+     * @return il cursore creato
+     */
+    GLFWcursor* initCostumCursor();
+
+    ///NOTA CHE QUESTE FUNZIONI SONO STATICHE PERCHE QUANDO VENGONO CHIAMATE DALLE
+    ///FUNZIONI CALL BACK DI GLFW SE NON STATICHE NON COMPILA E DA IL SEGUENTE ERRORE :
+    ///"invalid use of non-static member function"
+    /**
      * @brief framebuffer_size_callback funzione di callback per il resize del viewport, ovvero lo spazio di disegno
      * @param window oggetto finestra sul quale applicare il resize
      * @param width largezza viewport
@@ -47,7 +58,49 @@ private:
      */
     static void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
+    /**
+     * @brief mouse_callBack funzione che viene chiamata ogni volta che il mouse si muove ritornato la posizone del mouse
+     * @param window oggetto sul quale chiamare opera la funzione
+     * @param xpos  x posizone del mouse
+     * @param ypos  y posizione del mouse
+     */
+    static void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 
+    /**
+     * @brief scroll_callback funzione callback per ottenere l'ammontare di rotazione della rotella del mouse o del trackpad
+     * @param window
+     * @param xpos
+     * @param ypos
+     */
+    static void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
+    /**
+     * @brief cursor_enter_call_back funzione che avvisa se il courso entra o esce dalla finistra
+     * @param window
+     * @param entered
+     */
+    static void cursor_enter_call_back(GLFWwindow* window, int entered);
+
+    /**
+     * @brief mouse_button_callback funzione che i tasti del mouse
+     * @param button
+     * @param action
+     * @param mods
+     */
+    static void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+
+    /**
+     * @brief character_callback funzione per ricevere i tasti premuti nella metdoologia stream inout continuo
+     * @param window
+     * @param codepoint il tast premuto è dato sotto forma unicode standart
+     */
+    static void character_callback(GLFWwindow* window, unsigned int codepoint);
+
+
+    /**
+     * @brief processInput funzione per mettersi in ascolto degli input da tastiera sulla finestra
+     * @param window oggetto finestra
+     */
     void processInput(GLFWwindow* window);
 
     //oggetto finistra
@@ -56,11 +109,6 @@ private:
     //variabile per la finestra
     int mWidth;
     int mHeight;
-
-    float r;
-    float g;
-    float b;
-    float a;
 };
 
 #endif
