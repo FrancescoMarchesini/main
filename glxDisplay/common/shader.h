21b6436148bd3fa092a8d86af3fdb7ceb0034567
FrancescoMarchesini <marchesinifrancesco1@gmail.com>
2018-03-25 20:08:03 +0200

    aggiunto intestazione delle funzioni nel file shader.h

--- glxDisplay/common/shader.h
+++ glxDisplay/common/shader.h
@@ -15,8 +15,26 @@ class Shader
 {
     public:
         static Shader* create(const char * vertexPath, const char* fragmentPath, const char* geomtryPath = nullptr);
+
+        /**
+         * @brief use funzione per utilizzare gli shader caricati da dichiare prima del draw dell'oggeto
+         */
         void use();
+
+        //Semplici warp delle funzioni di utility di openGl per settare i parametri degli shader
+        //mancano tutti i get value....
         void setBool(std::string &name, bool value) const;
+        void setInt(const std::string &name, int value) const;
+        void setFloat(const std::string &name, float value) const;
+        void setVec2(const std::string &name, const glm::vec2 &value) const;
+        void setVec2(const std::string &name, float x, float y) const;
+        void setVec3(const std::string &name, const glm::vec3 &value) const;
+        void setVec3(const std::string &name, float x, float y, float z) const;
+        void setVec4(const std::string &name, const glm::vec4 &value) const;
+        void setVec4(const std::string &name, float x, float y, float z, float w);
+        void setMat2(const std::string &name, const glm::mat2 &mat) const;
+        void setMat3(const std::string &name, const glm::mat3 &mat) const;
+        void setMat4(const std::string &name, const glm::mat4 &mat) const;
 
     private:
         Shader();
