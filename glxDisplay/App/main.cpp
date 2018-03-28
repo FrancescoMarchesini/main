1a26ee617259972a3faf0a5a32211a754d432302
FrancescoMarchesini <marchesinifrancesco1@gmail.com>
2018-03-28 11:50:57 +0200

    Scritto main function

--- glxDisplay/App/main.cpp
+++ glxDisplay/App/main.cpp
@@ -1,11 +1,20 @@
-#include "glxDisplay.h"
+#include "App.h"
 #include <iostream>
 
 int main(int argv, char*argc[])
 {
 
-    glxDisplay* win = glxDisplay::create();
-    win->draw();
-    delete win;
+    App * my = App::init();
+    if(!my->setup())
+    {
+        printf("[LOG_MAIN]ok tutte le tue funzioni gl sono ok");
+    }
+
+    my->loop();
+
+    my->draw();
+
+    delete my;
+
     return 0;
 }
