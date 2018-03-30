b6e1b3ffbe24b358056c67979e4caffed31eeeb1
FrancescoMarchesini <marchesinifrancesco1@gmail.com>
2018-03-30 10:33:55 +0200

    aggiunto funzioni virtuali

--- glxDisplay/App/App.cpp
+++ glxDisplay/App/App.cpp
@@ -24,3 +24,59 @@ void App::clean()
 {
     printf("%sclean classe App\n", LOG_APP_INFO);
 }
+
+//--------------------------------------------------------------
+//funzioni virtuali per riceve gli input : mouse / tastiera
+//--------------------------------------------------------------
+void App::mouse_callback(double xpos, double ypos)
+{
+    printf("%s mouse : xpos = %f ypos = %f\n", LOG_APP_INFO, xpos, ypos);
+}
+
+void App::scroll_callback(double xoffset, double yoffset)
+{
+    printf("%s scroll : xpos = %0.3f ypos = %0.3f\n", LOG_APP_INFO, xoffset, yoffset);
+}
+
+void App::cursor_enter_call_back(int entered)
+{
+
+}
+
+void App::mouse_button_callback(int button, int action, int mods)
+{
+
+}
+
+void App::character_callback(unsigned int codepoint)
+{
+
+    static char result[6 + 1];
+
+    int length = wctomb(result, codepoint);
+    if (length == -1)
+        length = 0;
+
+    result[length] = '\0';
+
+    printf("%s char string : %s \n", LOG_APP_INFO, result);
+}
+
+void App::key_callback(int key, int scancode, int action, int mods)
+{
+    //nella funzione base sono gia implementati i seguenti input:
+    // ESC quidi APP
+    // F10 restore defualt size windows
+    // F11 full screen
+    // F12 iconizza finistra
+}
+
+void App::drop_callback(int count, const char **paths)
+{
+    int i;
+        for (i = 0;  i < count;  i++)
+        {
+          printf("%s Caricato file %s\n", LOG_APP_INFO, paths[i]);
+          //quad->initTexture(paths[i]);
+        };
+}
--- glxDisplay/App/App.h
+++ glxDisplay/App/App.h
@@ -18,9 +18,73 @@ public:
      */
     App(void);
 
+    /**
+     * @brief setup funzione nella quale fare il esetup del disegno
+     */
     virtual void setup() override;
+
+    /**
+     * @brief loop funzione nella quale disegnare : main loop
+     */
     virtual void loop() override;
+
+    /**
+     * @brief clean funzione per pulire gli oggetti di gl prima di chiudere
+     */
     virtual void clean() override;
+
+    //-------------------------------------------------------------------------
+    //funzioni per ricevere input----------------------------------------------
+    //-------------------------------------------------------------------------
+    /**
+     * @brief mouse_callback get mouse position insied window
+     * @param xpos get xpos
+     * @param ypos get ypos
+     */
+    virtual void mouse_callback(double xpos, double ypos) override;
+
+    /**
+     * @brief scroll_callback get scroll mouse
+     * @param xoffset get xoffset [0-1] trackpad
+     * @param yoffset get xoffset [0-1] mouse
+     */
+    virtual void scroll_callback(double xoffset, double yoffset) override;
+
+    /**
+     * @brief cursor_enter_call_back get if curson enter on window
+     * @param entered in if yes or no
+     */
+    virtual void cursor_enter_call_back(int entered) override;
+
+    /**
+     * @brief mouse_button_callback get mouse botton press
+     * @param button which botton left / right / center
+     * @param action
+     * @param mods  if pressed / released
+     */
+    virtual void mouse_button_callback(int button, int action, int mods);
+
+    /**
+     * @brief character_callback get the carater pressed
+     * @param codepoint codepoint definition
+     */
+    virtual void character_callback(unsigned int codepoint);
+
+    /**
+     * @brief key_callback get key pressed
+     * @param key wich key GLFW_KEY_X
+     * @param scancode
+     * @param action
+     * @param mods pressed / release
+     */
+    virtual void key_callback(int key, int scancode, int action, int mods);
+
+    /**
+     * @brief drop_callback drop file inside window
+     * @param count
+     * @param paths
+     */
+    virtual void drop_callback(int count, const char** paths);
 };
 
 #endif
