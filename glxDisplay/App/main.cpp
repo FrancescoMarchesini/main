95a6b84ffa2c3dea8917db3b020c284abb617cdb
FrancescoMarchesini <marchesinifrancesco1@gmail.com>
2018-04-03 17:40:58 +0200

    aggiunto classe gst al main

--- glxDisplay/App/main.cpp
+++ glxDisplay/App/main.cpp
@@ -1,7 +1,10 @@
 #include "App.h"
+#include "../common/Gstreamer/gstreamer.h"
 
 int main(int argv, char*argc[])
 {
+    GStreamer cam;
+
     App app;
     glxDisplay *display = &app;
     display->draw(app);
